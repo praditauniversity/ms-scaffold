98cc295d80acaa8d142b6a897a1b32c67738616a
brianmikht <110759186+brianmikht@users.noreply.github.com>
2022-11-03 13:41:29 +0700

    change variable userID

--- api/models/domain/scaffold.go
+++ api/models/domain/scaffold.go
@@ -1,18 +1,14 @@
 package domain
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type Scaffold struct {
 	gorm.Model
 	Name        string    `json:"name" gorm:"type:varchar(255);not null"`
-	Description string    `json:"description" gorm:"type:varchar(255);not null"`
-	StartTime   time.Time `json:"start_time"`
-	EndTime     time.Time `json:"end_time"`
-	User_id     uint64    `json:"user_id" gorm:"type:uint;not null"`
+	Description string    `json:"description" gorm:"type:text;not null"`
+	UserID     uint64    `json:"user_id" gorm:"type:uint;not null"`
 	UpdatedBy   string    `json:"updated_by"`
 	DeletedBy   string    `json:"deleted_by"`
 }
--- api/models/web/scaffold_request.go
+++ api/models/web/scaffold_request.go
@@ -3,7 +3,7 @@ package web
 type ScaffoldRequest struct {
 	Name        string `json:"name" binding:"required"`
 	Description string `json:"description" binding:"required"`
-	User_id     uint64 `json:"user_id" binding:"required"`
+	UserID      uint64 `json:"user_id" binding:"required"`
 	UpdatedBy   string `json:"updated_by"`
 	DeletedBy   string `json:"deleted_by"`
 }
@@ -12,7 +12,7 @@ type ScaffoldUpdateRequest struct {
 	ID          uint   `json:"id" binding:"required"`
 	Name        string `json:"name" binding:"required"`
 	Description string `json:"description" binding:"required"`
-	User_id     uint64 `json:"user_id" binding:"required"`
+	UserID      uint64 `json:"user_id" binding:"required"`
 	UpdatedBy   string `json:"updated_by" binding:"required"`
 	DeletedBy   string `json:"deleted_by"`
 }
